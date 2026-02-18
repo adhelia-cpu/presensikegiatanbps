1ba875b3525cce5e34bce6dbbeff7112089c55e7
Adhelia <adheliaamandasilvy@gmai.com>
2026-02-18 15:45:02 +0700

    Update: Login dengan Card Layout dan BG Biru Muda

--- src/app/page.js
+++ src/app/page.js
@@ -1,16 +1,21 @@
 import Image from "next/image"; 
 export default function LoginPage() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-[#769BFF] p-10">
+    <div className="flex min-h-screen items-center justify-center bg-blue-100 p-10">
+      <div className="bg-white w-full max-w-md p-8 rounded-[30px] shadow-xl flex flex-col items-center">
       <div className="flex flex-col items-center w-full max-w-sm">
-        <div className="mb-6">
+        <div className="mb-6"> 
           <img 
             src="/logobps2.png" 
             alt="Logo BPS"
-            className="w-40 h-auto drop-shadow-md" 
+            className="w-36 h-auto drop-shadow-xl" 
           />
         </div>
-        <h1 className="text-white text-xl font-black tracking-widest mb-6 font-sans">
+        <h2 className="text-gray-600 text-sm font-bold tracking-widest mb-2">
+          BADAN PUSAT STATISTIK
+        </h2>
+
+        <h1 className="text-blue-600 text-xl font-black tracking-[0.2em] mb-8 font-sans text-center">
           SELAMAT DATANG
         </h1>
 
@@ -26,14 +31,15 @@ export default function LoginPage() {
           className="w-full bg-white border border-black-300 rounded-full px-6 py-4 mb-8 focus:outline-none font-bold placeholder:text-gray-400 shadow-sm"
         />
 
-        <button className="bg-blue-600 hover:bg-blue-700 text-white font-bold py-3 px-12 rounded-full text-lg mb-6 shadow-lg transition-all w-full">
-          Login
+       <button className="bg-[#1e5eff] hover:bg-blue-700 text-white font-black py-3 px-12 rounded-full text-lg mb-4 shadow-lg transition-transform active:scale-95 w-full">
+          LOGIN
         </button>
 
         <p className="font-bold text-gray-700">
           Belum Punya Akun? <span className="underline cursor-pointer text-blue-600">Daftar</span>
         </p>
 
+      </div>
       </div>
     </div>
   );
